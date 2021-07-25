58b7770528fb12c352f986920f85b3e86628d675
MAKE OUT HILL <felipeh2002@gmail.com>
2021-07-25 02:27:13 -0500

    Se agregaron algunas animaciones

---
+++ js/animated.js
@@ -0,0 +1,6 @@
+window.addEventListener('scroll', () => {
+    let objetoAnimar = document.querySelector('.animated-scroll');
+    let posicionObjeto = objetoAnimar.getBoundingClientRect().top;
+
+    
+});
