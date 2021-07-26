9c93397bdad5fb6b20620f317a5f6992d1c7dd9c
MAKE OUT HILL <felipeh2002@gmail.com>
2021-07-25 23:43:59 -0500

    Se terminaron de agregar algunos detalles

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
-addEventListener('DOMContentLoaded', () => {
-    const btnMenu = document.querySelector('.navbar__btn')
-    if(btnMenu){
-        btnMenu.addEventListener('click', ()=>{
-            const menuItems = document.querySelector('.navbar__menu')
-            
-            menuItems.classList.toggle('show')
+addEventListener("DOMContentLoaded", () => {
+  const btnMenu = document.querySelector(".navbar__btn");
+  if (btnMenu) {
+    btnMenu.addEventListener("click", () => {
+      const menuItems = document.querySelector(".navbar__menu");
 
+      menuItems.classList.toggle("show");
 
-            const menu = document.querySelector('.navbar__contenedor')
-            menu.classList.toggle('show__menu')
+      const menu = document.querySelector(".navbar__contenedor");
+      menu.classList.toggle("show__menu");
 
-            const body = document.querySelector('.landing')
-            body.classList.toggle('no_scroll')
-
-           
-        } )
-    }
-})
+      const body = document.querySelector(".landing");
+      body.classList.toggle("no_scroll");
+    });
+  }
+});
